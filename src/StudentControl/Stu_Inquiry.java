bc90e7fe4bd3fb81de8f3bd48fbb691b7a5a0e55
gyeol <gyeol9012@naver.com>
2024-08-02 17:03:10 +0900

    고유번호,이름 조회 (결)

---
+++ src/StudentControl/Stu_Inquiry.java
@@ -0,0 +1,33 @@
+package StudentControl;
+
+public class Stu_Inquiry {
+    private String studentId;
+    private String studentName;
+
+    public Stu_Inquiry(String seq, String studentName) {
+        this.studentId = seq;
+        this.studentName = studentName;
+    }
+
+    // Getter
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId=studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+    public void setStudentName(String studentName) {
+        this.studentName=studentName;
+    }
+
+    @Override
+    public String toString(){
+        return "(수강생) ID: " + studentId+" / 이름 : "+studentName;
+    }
+
+}
