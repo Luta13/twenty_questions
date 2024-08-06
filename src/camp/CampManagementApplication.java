96647b4f6b3ef0338acc1d6782df1486c539d871
MinJun Song <j34679@naver.com>
2024-08-06 19:46:39 +0900

    inquiryview

--- src/camp/CampManagementApplication.java
+++ src/camp/CampManagementApplication.java
@@ -365,7 +365,7 @@ public class CampManagementApplication {
             System.out.println("점수 관리 실행 중...");
             System.out.println("1. 수강생의 과목별 시험 회차 및 점수 등록");
             System.out.println("2. 수강생의 과목별 회차 점수 수정");
-            System.out.println("3. 수강생의 특정 과목 회차별 등급 조회");
+            System.out.println("3. 수강생 등급 조회");
             System.out.println("4. 메인 화면 이동");
             System.out.print("관리 항목을 선택하세요...");
             int input = sc.nextInt();
@@ -374,9 +374,8 @@ public class CampManagementApplication {
             switch (input) {
                 case 1 -> createScore(); // 수강생의 과목별 시험 회차 및 점수 등록
                 case 2 -> updateRoundScoreBySubject(); // 수강생의 과목별 회차 점수 수정
-                case 3 -> inquireRoundGradeBySubject(); // 수강생의 특정 과목 회차별 등급 조회
-                case 4 -> inquireAverageGradeBySubject();
-                case 5 -> flag = false; // 메인 화면 이동
+                case 3 -> displayinquiryView(); // 조회뷰로 이동
+                case 4 -> flag = false; // 메인 화면 이동
                 default -> {
                     System.out.println("잘못된 입력입니다.\n메인 화면 이동...");
                     flag = false;
@@ -548,6 +547,22 @@ public class CampManagementApplication {
         System.out.println("점수 수정이 완료되었습니다.");
     }
 
+    private static void displayinquiryView()
+    {
+        System.out.println("==================================");
+        System.out.println("수강생의 등급 조회 실행 중...");
+        System.out.println("1. 수강생의 특정 과목 회차별 등급 조회");
+        System.out.println("2. 수강생의 과목별 평균 등급 조회");
+        System.out.print("관리 항목을 선택하세요: ");
+
+        switch(sc.nextInt())
+        {
+            case 1 -> inquireRoundGradeBySubject();
+            case 2 -> inquireAverageGradeBySubject();
+        }
+
+
+    }
 
     // 수강생의 특정 과목 회차별 등급 조회
     private static void inquireRoundGradeBySubject() {
